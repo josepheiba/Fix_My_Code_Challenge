2b02d880d6a0ac9cfcdef71ba5ebd141edafda00
Joseph Eiba <josepheiba@icloud.com>
2024-05-15 17:25:40 +0100

    ok

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,6 +7,7 @@ class square():
     width = 0
 
     def __init__(self, *args, **kwargs):
+        """ square init """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
