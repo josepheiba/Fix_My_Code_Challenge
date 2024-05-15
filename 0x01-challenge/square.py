9c28dd467e92a964ece6e129c9ae336226d2b80c
Joseph Eiba <josepheiba@icloud.com>
2024-05-15 17:47:36 +0100

    ok

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,22 +4,16 @@ Code defining square class and its methods for calculating area and perimeter.
 """
 
 
-class Square:
+class Square():
     """
     A class representing a square.
-
-    Attributes:
-        width (int): The width of the square.
     """
     width = 0
     height = 0
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         Initializes the square with specified attributes.
-
-        Args:
-            **kwargs: Arbitrary keyword arguments to set attributes.
         """
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -27,26 +21,17 @@ class Square:
     def area_of_my_square(self):
         """
         Calculates the area of the square.
-
-        Returns:
-            int: The area of the square.
         """
         return self.width * self.height
 
     def perimeter_of_my_square(self):
         """
         Calculates the perimeter of the square.
-
-        Returns:
-            int: The perimeter of the square.
         """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """
         Returns a string representation of the square.
-
-        Returns:
-            str: String representation of the square's width.
         """
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
